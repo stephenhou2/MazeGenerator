ce85251bb4a987362d479a06d80bc8d3508ad98c
stephenhou2 <stephenhou2@163.com>
2021-12-09 22:58:15 +0800

    update

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -54,6 +54,23 @@ public class MapData
                 _mapCellType[i, j] = MapDef.CELL_TYPE_ROOM;
             }
         }
+
+        for (int i = room.Left-1; i <= room.Right+1; i++)
+        {
+            _mapCellType[i,room.Bottom-1] = MapDef.CELL_TYPE_WALL;
+        }
+        for (int i = room.Left - 1; i <= room.Right + 1; i++)
+        {
+            _mapCellType[i,room.Top+1] = MapDef.CELL_TYPE_WALL;
+        }
+        for (int i = room.Bottom - 1; i <= room.Top + 1; i++)
+        {
+            _mapCellType[room.Left-1, i] = MapDef.CELL_TYPE_WALL;
+        }
+        for (int i = room.Bottom - 1; i <= room.Top + 1; i++)
+        {
+            _mapCellType[room.Right+1, i] = MapDef.CELL_TYPE_WALL;
+        }
     }
 
     public bool TryAddRoom(Vector2Int pos, int halfWidth, int halfHeight)
@@ -78,11 +95,55 @@ public class MapData
         return _mapCellType[x, y] == MapDef.CELL_TYPE_WALL;
     }
 
-    private Vector2Int[] RandomNeighbors()
+    private int[] RandomNeighbors()
     {
         int seed = Random.Range(0, 4);
         return MapDef.NEIGHBORS_POOL[seed];
     }
+
+    private void TryChangeToWall(Vector2Int pos)
+    {
+        if(_mapCellType[pos.x,pos.y] == MapDef.CELL_TYPE_FLOOR)
+        {
+            _mapCellType[pos.x, pos.y] = MapDef.CELL_TYPE_WALL;
+        }
+    }
+
+    private bool TryAddCorridor(Vector2Int from,int direction)
+    {
+        Vector2Int offset = MapDef.OFFSET_MAP[direction];
+        Vector2Int newPos1 = from + offset;
+        Vector2Int newPos2 = from + 2 * offset;
+        if(_mapCellType[newPos1.x, newPos1.y] == MapDef.CELL_TYPE_FLOOR
+             && _mapCellType[newPos2.x, newPos2.y] == MapDef.CELL_TYPE_FLOOR)
+        {
+            _mapCellType[newPos1.x, newPos1.y] = MapDef.CELL_TYPE_CORRIDOR;
+            _mapCellType[newPos2.x, newPos2.y] = MapDef.CELL_TYPE_CORRIDOR;
+
+            switch(direction)
+            {
+                case MapDef.DIRECTION_UP:
+                case MapDef.DIRECTION_DOWN:
+                    TryChangeToWall(from + MapDef.LEFT);
+                    TryChangeToWall(from + MapDef.RIGHT);
+                    TryChangeToWall(newPos1 + MapDef.LEFT);
+                    TryChangeToWall(newPos1 + MapDef.RIGHT);
+                    break;
+                case MapDef.DIRECTION_LEFT:
+                case MapDef.DIRECTION_RIGHT:
+                    TryChangeToWall(from + MapDef.UP);
+                    TryChangeToWall(from + MapDef.DOWN);
+                    TryChangeToWall(newPos1 + MapDef.UP);
+                    TryChangeToWall(newPos1 + MapDef.DOWN);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+        return false;
+    }
+
 
     /// <summary>
     /// 洪水填充算法生成迷宫走廊
@@ -92,45 +153,37 @@ public class MapData
     {
         _toHandle.Clear();
         _toHandle.Enqueue(start);
-        _mapCellType[start.x, start.y] = MapDef.CELL_TYPE_EMPTY;
+        _mapCellType[start.x, start.y] = MapDef.CELL_TYPE_CORRIDOR;
 
         while (_toHandle.Count > 0)
         {
             Vector2Int pos = _toHandle.Dequeue();
-            Vector2Int[] neighbors = RandomNeighbors();
+            int[] neighbors = RandomNeighbors();
             for(int i=0;i< neighbors.Length;i++)
             {
-                // 走廊旁边要留一个格子当作墙，所以要向周围扩展两步
-                if (CheckIsWall(pos.x + neighbors[i].x,pos.y+ neighbors[i].y) 
-                    && CheckIsWall(pos.x + 2* neighbors[i].x,pos.y + 2* neighbors[i].y))
-                    {
-                        Vector2Int newPos1 = pos + neighbors[i];
-                        Vector2Int newPos2 = pos + 2*neighbors[i];
-                        _mapCellType[newPos1.x, newPos1.y] = MapDef.CELL_TYPE_EMPTY;
-                        _mapCellType[newPos2.x, newPos2.y] = MapDef.CELL_TYPE_EMPTY;
-                        _toHandle.Enqueue(newPos2);
-                        break;
-                    }
+                int direction = neighbors[i];
+                if (TryAddCorridor(pos, direction))
+                {
+                    _toHandle.Enqueue(pos+ 2*MapDef.OFFSET_MAP[direction]);
+                    break;
+                }
             }
         }
     }
 
     public void FloodFillMaze()
     {
-        for (int i = 1; i < _workSpaceWidth - 1; i++)
-        {
-            for (int j = 1; j < _workSpaceHeight - 1; j++)
-            {
-                if (_mapCellType[i, j] == MapDef.CELL_TYPE_WALL
-                    && _mapCellType[i - 1, j] == MapDef.CELL_TYPE_WALL
-                    && _mapCellType[i + 1, j] == MapDef.CELL_TYPE_WALL
-                    && _mapCellType[i, j - 1] == MapDef.CELL_TYPE_WALL
-                    && _mapCellType[i, j + 1] == MapDef.CELL_TYPE_WALL)
-                {
-                    FillMaze(new Vector2Int(i, j));
-                }
-            }
-        }
+        //for (int i = 0; i < _workSpaceWidth; i++)
+        //{
+        //    for (int j = 0; j < _workSpaceHeight; j++)
+        //    {
+        //        if (_mapCellType[i, j] == MapDef.CELL_TYPE_FLOOR)
+        //        {
+        //            FillMaze(new Vector2Int(i, j));
+        //        }
+        //    }
+        //}
+        FillMaze(new Vector2Int(1, 1));
     }
 
     public void InitializeMapData(int mapWidth,int mapHeight)
@@ -139,13 +192,30 @@ public class MapData
         _workSpaceHeight = mapHeight;
         _mapCellType = new int[mapWidth, mapHeight];
 
-        // 先全部初始化为墙
+        // 先全部初始化为地板
         for (int i = 0; i < mapWidth; i++)
         {
             for (int j = 0; j < mapHeight; j++)
             {
-                _mapCellType[i, j] = MapDef.CELL_TYPE_WALL;
+                _mapCellType[i, j] = MapDef.CELL_TYPE_FLOOR;
             }
+        }
+
+        for (int i = 0; i < _workSpaceWidth; i++)
+        {
+            _mapCellType[i, 0] = MapDef.CELL_TYPE_WALL;
+        }
+        for (int i = 0; i < _workSpaceWidth; i++)
+        {
+            _mapCellType[i, _workSpaceHeight - 1] = MapDef.CELL_TYPE_WALL;
+        }
+        for (int i = 0; i < _workSpaceHeight; i++)
+        {
+            _mapCellType[0, i] = MapDef.CELL_TYPE_WALL;
+        }
+        for (int i = 0; i < _workSpaceHeight; i++)
+        {
+            _mapCellType[_workSpaceWidth - 1, i] = MapDef.CELL_TYPE_WALL;
         }
     }
 
--- Assets/Scripts/MapDef.cs
+++ Assets/Scripts/MapDef.cs
@@ -4,46 +4,66 @@ using UnityEngine;
 
 public static class MapDef
 {
-    public const int CELL_TYPE_EMPTY = 0;
+    public const int CELL_TYPE_FLOOR = 0;
     public const int CELL_TYPE_ROOM = 1;
     public const int CELL_TYPE_WALL = 2;
+    public const int CELL_TYPE_CORRIDOR = 3;
 
-    private static Vector2Int[] _neighbors_1 = new Vector2Int[]
-{
-            new Vector2Int(-1,0),
-            new Vector2Int(0,-1),
-            new Vector2Int(1,0),
-            new Vector2Int(0,1),
-};
 
-    private static Vector2Int[] _neighbors_2 = new Vector2Int[]
+    public const int DIRECTION_UP = 0;
+    public const int DIRECTION_DOWN = 1;
+    public const int DIRECTION_LEFT = 2;
+    public const int DIRECTION_RIGHT = 3;
+
+    private static int[] _neighbors_1 = new int[]
     {
-            new Vector2Int(0,-1),
-            new Vector2Int(1,0),
-            new Vector2Int(0,1),
-            new Vector2Int(-1,0),
+            DIRECTION_UP,
+            DIRECTION_DOWN,
+            DIRECTION_LEFT,
+            DIRECTION_RIGHT,
     };
 
-    private static Vector2Int[] _neighbors_3 = new Vector2Int[]
+    private static int[] _neighbors_2 = new int[]
     {
-                new Vector2Int(1,0),
-                new Vector2Int(0,1),
-                new Vector2Int(-1,0),
-                new Vector2Int(0,-1),
-    };
-    private static Vector2Int[] _neighbors_4 = new Vector2Int[]
-    {
-                new Vector2Int(0,1),
-                new Vector2Int(-1,0),
-                new Vector2Int(0,-1),
-                new Vector2Int(1,0),
+            DIRECTION_DOWN,
+            DIRECTION_LEFT,
+            DIRECTION_RIGHT,
+            DIRECTION_UP,
     };
 
-    public static List<Vector2Int[]> NEIGHBORS_POOL = new List<Vector2Int[]>
+    private static int[] _neighbors_3 = new int[]
+    {
+            DIRECTION_LEFT,
+            DIRECTION_RIGHT,
+            DIRECTION_UP,
+            DIRECTION_DOWN,
+    };
+    private static int[] _neighbors_4 = new int[]
+    {
+            DIRECTION_RIGHT,
+            DIRECTION_UP,
+            DIRECTION_DOWN,
+            DIRECTION_LEFT,
+    };
+
+    public static List<int[]> NEIGHBORS_POOL = new List<int[]>
     {
         _neighbors_1,
         _neighbors_2,
         _neighbors_3,
         _neighbors_4
+    };
+
+    public static Vector2Int UP = new Vector2Int(0, 1);
+    public static Vector2Int DOWN = new Vector2Int(0, -1);
+    public static Vector2Int LEFT = new Vector2Int(-1, 0);
+    public static Vector2Int RIGHT = new Vector2Int(1, 0);
+
+    public static Vector2Int[] OFFSET_MAP = new Vector2Int[]
+    {
+        UP, //up
+        DOWN , //down
+        LEFT, //left
+        RIGHT, //right
     };
 }
